9111df321afd1bae45aa306c5f136b7e6e028c42
Bruna Campos <92brunacampos@gmail.com>
2021-08-27 14:06:37 -0300

    exercicio de variaveis, let, const, template literals e ternario

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+let idade = 29;
+// console.log(idade);
+let altura = 1.69;
+// console.log(altura);
+let nomeCompleto = "Bruna Campos Ferreira";
+// console.log(nomeCompleto);
+let apelido = "Brubru";
+// console.log(apelido);
+let dataNascimento = "17/08/1992";
+// console.log(dataNascimento);
+let localNascimento = "Rio de Janeiro/ RJ";
+// console.log(localNascimento);
+let cidadeAtual = "Novo Hamburgo/RS";
+// console.log(cidadeAtual);
+trabalhando = false;
+
+apresentacao = `Meu nome é ${nomeCompleto}, sou conhecida como ${apelido} e tenho ${idade} anos. Nasci no dia ${dataNascimento}, na cidade de ${localNascimento} e atualmente resido em ${cidadeAtual}. Tenho ${altura}m de altura e atualmente estou ${trabalhando ? "empregada" : "desempregada"}.`;
+
+console.log(apresentacao);
