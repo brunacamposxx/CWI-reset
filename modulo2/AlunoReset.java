3e84d7ed66ca50a4c44da6e2cb2a36f2da1fa549
Bruna Campos <92brunacampos@gmail.com>
2021-09-07 19:52:26 -0300

    cria class AlunoReset

---
+++ modulo2/AlunoReset.java
@@ -0,0 +1,9 @@
+class AlunoReset {
+	public static void main(String[] args){
+		System.out.println("Sou aluno do Level 1 do CWI Reset!");
+	}
+};
+
+// javac AlunoReset.java para compilar, criando arquivo AlunoReset.class
+// java AlunoReset para imprimir no terminal 
+// ou Run Code com ctrl+alt+n;
