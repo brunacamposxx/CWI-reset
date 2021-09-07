b3acb15938e6db0126ef3af955c666cd7fb788e3
Bruna Campos <92brunacampos@gmail.com>
2021-09-06 23:59:58 -0300

    Meu primeiro Hello World em java

---
+++ modulo2/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+
+	public static void main (String[] args){
+		System.out.println("Hello, World");
+	}
+}
+
+// HelloWorld - diferente do JS, começar o nome da classe/arquivo com letra maiuscula;
+// compliar o java no terminal com o comando: javac NomeDoArquivo.java
+// criará um arquivo com o NomeDoArquivo.class
+// para executar: java NomeDoArquivo
+// nesse caso: java HelloWorld
