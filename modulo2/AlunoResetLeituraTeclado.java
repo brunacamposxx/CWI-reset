235fe71040fa229ad3fdc1ba23213df56a4b3e07
Bruna Campos <92brunacampos@gmail.com>
2021-09-08 17:26:20 -0300

    adiciona AlunoResetLeituraTeclado.class

--- modulo2/AlunoResetLeituraTeclado.java
+++ modulo2/AlunoResetLeituraTeclado.java
@@ -11,9 +11,3 @@ public class AlunoResetLeituraTeclado {
 		scan.close();
 	}	
 };
-
-// Exercício:
-// Vamos refazer o exercício da aula 3 usando a classe Scanner:
-// Crie um arquivo chamado AlunoResetLeituraTeclado.java
-// Implemente um programa que solicite para o usuário digitar o próprio nome. Ao final imprima na tela o texto “Sou aluno do Level 1 do CWI Reset! Me chamo {SEU NOME}”
-// Compile e execute o programa manualmente ou utilize a IDE para facilitar
