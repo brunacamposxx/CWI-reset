015e00bf5e4f3704009741643fb8bda0e38379dc
Bruna Campos <92brunacampos@gmail.com>
2021-09-09 16:24:39 -0300

    loop for

---
+++ modulo2/For.java
@@ -0,0 +1,15 @@
+public class For {
+
+		public static void main(String[] args){
+			
+			System.out.println("Contando de 1 a 5");
+	
+			for(int index = 0; index < 5; index += 1 ){
+				System.out.println("Valor de index: " + index);
+			}
+	
+		}
+	
+	
+
+}
