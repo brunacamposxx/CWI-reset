7155faee3687c4634a89a5f2c56858b28ad70e59
Bruna Campos <92brunacampos@gmail.com>
2021-10-17 16:14:28 -0300

    delete arquivos do level 1

--- modulo2/AlunoResetComParametro.java
+++
@@ -1,8 +0,0 @@
-class AlunoResetComParametro {
-	public static void main(String[] args){
-		System.out.println("Sou aluno do Level 1 do CWI Reset! Me chamo " + args[0]);
-	}	
-}
-
-// javac AlunoResetComParametro.java
-// java AlunoResetComParametro "Bruna Campos";
